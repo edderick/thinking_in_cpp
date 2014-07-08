e5395313cdc1f1b263b326c28d29d3353d7f19c6
Edward Seabrook <edderick@live.co.uk>
2014-07-08 17:38:22 +0100

    Added files for Chapter 3 Ex 1

---
+++ chapter_3/ch3ex1.h
@@ -0,0 +1,12 @@
+#ifndef _CH3EX1_H_
+#define _CH3EX1_H_
+
+void func1(); 
+char func2();
+int func3(); 
+float func4(); 
+void func5(char); 
+void func6(int); 
+void func7(float); 
+
+#endif
---
+++ chapter_3/ch3ex1b.cpp
@@ -0,0 +1,13 @@
+#include "ch3ex1.h"
+
+int main() {
+    
+    func1();
+    func2();
+    func3();
+    func4();
+    func5('0');
+    func6(0);
+    func7(0.0);
+
+}
