d1fe5df8bda348562eb1c7a1344aa87c346ed065
Edward Seabrook <edderick@live.co.uk>
2014-09-07 16:15:54 +0100

    Removed unneeded solutions

--- chapter_2/ch2ex1.cpp
+++
@@ -1,8 +0,0 @@
-//: C02:Hello.cpp
-// Saying Hello with C++
-#include <iostream> // Stream declarations
-using namespace std;
-int main() {
-    cout << "Hello, World! I am Edward and I am "
-        << 22 << " years old!" << endl;
-} ///:~ 
